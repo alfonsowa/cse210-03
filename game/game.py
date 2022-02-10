44468a93cdfbcb0ba2a6e791b2b9ae3a767366d1
alfonsowa <alfonsowaracaoa@gmal.com>
2022-02-09 23:14:47 -0300

    add comments

--- game/game.py
+++ game/game.py
@@ -3,7 +3,26 @@ from game.player import Player
 from game.display import Display
 
 class Game:
+
+    """ The Game Class is the director from the jumper Game,
+    
+     From here we will control the sequence from the game
+     
+     Atributes:
+     
+     still_playing (boolean): Whether or not to keep playing.
+     player: Identify player.
+     word: choice the word on a TXT file.
+     secret_word: will put and hide the letters during the game.
+     display: will display the letter and  lives from the parachute man drawing.
+     """
     def __init__(self):
+        """Construct a new jumper game.
+     
+        Args:
+          self (game): an instance of game
+        """
+
         self.still_playing: bool
         self.player = Player()
         self.word = Word()
@@ -11,6 +30,11 @@ class Game:
         self.display = Display(self.secret_word, self.player)
 
     def main(self):
+        """Start the game , game loop
+        
+        Args:
+             Self (game): an instance of game. """
+
         self.still_playing = True
         while self.player.lives > 0 and self.still_playing == True:
             guess = input("Guess a letter [A-Z] or type quit to quit: ")
